bf6e4339985a1c89a416e4bd01ed12735f09f6b5
siddhant-shekhar <104164130+siddhant-shekhar@users.noreply.github.com>
2023-09-15 16:14:50 +0000

    added model

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 import numpy as np
 import sklearn
 
 app = Flask(__name__)
 
-model = pickle.load(open('model_ran_for.pkl', 'rb'))
+model = pickle.load(open('final_model.pkl', 'rb'))
 
 
 @app.route("/")
@@ -13,27 +13,37 @@ def hello():
   return render_template('home.html')
 
 
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route("/predict", methods=['POST'])
 def predict():
-  int_features = [int(x) for x in request.form.values()]
-  final = [np.array(int_features)]
-  print(int_features)
-  print(final)
-  prediction = model.predict_proba(final)
-  output = '{0:.{1}f}'.format(prediction[0][1], 2)
+  no_of_dependents = (request.form.get('no_of_dependents'))
+  education = (request.form.get('education'))
+  self_employed = (request.form.get('self_employed'))
+  income_annum = (request.form.get('income_annum'))
+  loan_amount = (request.form.get('loan_amount'))
+  loan_term = (request.form.get('loan_term'))
+  cibil_score = (request.form.get('cibil_score'))
+  residential_assets_value = (request.form.get('residential_assets_value'))
+  commercial_assets_value = (request.form.get('commercial_assets_value'))
+  luxury_assets_value = (request.form.get('luxury_assets_value'))
+  bank_asset_value = (request.form.get('bank_asset_value'))
 
-  if output > str(0.5):
-    return render_template(
-      'forest_fire.html',
-      pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.
-      format(output),
-      bhai="kuch karna hain iska ab?")
+  prediction = model.predict(
+    np.asarray([
+      no_of_dependents, education, self_employed, income_annum, loan_amount,
+      loan_term, cibil_score, residential_assets_value,
+      commercial_assets_value, luxury_assets_value, bank_asset_value
+    ]).reshape(1, -1))
+
+  if (prediction[0] == 1):
+    return render_template('prediction_no.html')
   else:
-    return render_template(
-      'forest_fire.html',
-      pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(
-        output),
-      bhai="Your Forest is Safe for now")
+    return render_template('prediction_yes.html')
+
+
+@app.route("/form_info", methods=['GET', 'POST'])
+def form_info():
+  answers = dict(request.form)
+  return jsonify(answers)
 
 
 if __name__ == "__main__":
